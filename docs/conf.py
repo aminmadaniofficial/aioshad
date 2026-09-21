d0c1e985c2bc0207a1979d7674ea597cac4142ef
Amin Madani <aminmadani112@gmail.com>
2026-09-21 17:41:43 +0330

    bump: release version 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "aioshad"
 copyright = "2026, Mohammadamin Madani"
 author = "Mohammadamin Madani"
-release = "0.1.0"
+release = "0.1.1"
 
 extensions = [
     "sphinx.ext.autodoc",
